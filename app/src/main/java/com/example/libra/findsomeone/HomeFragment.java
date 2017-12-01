83856f6fdcf47e45d5f8101aad7c1731f00f28ec
Watchara <Watchara.che@gmail.com>
2017-12-01 15:48:41 +0700

    add onclick function in tab bar

---
+++ app/src/main/java/com/example/libra/findsomeone/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.example.libra.findsomeone;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HomeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    private View mView;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+    }
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        return mView;
+    }
+
+}
--- app/src/main/java/com/example/libra/findsomeone/MainActivity.java
+++ app/src/main/java/com/example/libra/findsomeone/MainActivity.java
@@ -1,13 +1,107 @@
 package com.example.libra.findsomeone;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
+
+    private enum Tab {Home, History, QRScanner, Event, Setting}
+
+
+    private Tab mCurrentTab;
+
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+
+    private List<ImageView> mTabBar;
+    @BindView(R.id.home_tab)
+    ImageView mHomeTab;
+    @BindView(R.id.history_tab)
+    ImageView mHistoryTab;
+    @BindView(R.id.qr_scanner_tab)
+    ImageView mQRScannerTab;
+    @BindView(R.id.event_tab)
+    ImageView mEventTab;
+    @BindView(R.id.setting_tab)
+    ImageView mSettingTab;
+
+    private List<Fragment> mPages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initializeTabBar();
+    }
+
+    private void initializeTabBar() {
+        mTabBar = new ArrayList<>();
+        mTabBar.add(mHomeTab);
+        mTabBar.add(mHistoryTab);
+        mTabBar.add(mQRScannerTab);
+        mTabBar.add(mEventTab);
+        mTabBar.add(mSettingTab);
+
+        setSelectedTab(mCurrentTab = Tab.Home);
+    }
+
+    private void initializeTabFragmentLayout() {
+        mPages = new ArrayList<>();
+
+
+    }
+
+    public void onClickTabBar(View v) {
+        switch (v.getId()) {
+            case R.id.home_tab: {
+                if (mCurrentTab == Tab.Home) return;
+                setSelectedTab(Tab.Home);
+            }
+            break;
+            case R.id.history_tab: {
+                if (mCurrentTab == Tab.History) return;
+                setSelectedTab(Tab.History);
+            }
+            break;
+            case R.id.qr_scanner_tab: {
+                if (mCurrentTab == Tab.QRScanner) return;
+                setSelectedTab(Tab.QRScanner);
+            }
+            break;
+            case R.id.event_tab: {
+                if (mCurrentTab == Tab.Event) return;
+                setSelectedTab(Tab.Event);
+            }
+            break;
+            case R.id.setting_tab: {
+                if (mCurrentTab == Tab.Setting) return;
+                setSelectedTab(Tab.Setting);
+            }
+            break;
+        }
+    }
+
+    private void setSelectedTab(Tab selectedTab) {
+        mTabBar.get(mCurrentTab.ordinal()).setColorFilter(ContextCompat.getColor(this, R.color.colorUnSelectedTab));
+        mTabBar.get(selectedTab.ordinal()).setColorFilter(ContextCompat.getColor(this, R.color.colorSelectedTab));
+        mCurrentTab = selectedTab;
     }
 }
